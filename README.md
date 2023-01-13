99b8fa097bf4b6f39a4c509ad4405a32d75ae917
tinkuhari <47702435+tinkuhari@users.noreply.github.com>
2023-01-13 01:00:09 -0500

    updated analysis script and README files

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
 # R_week3
+
+# Details on step involved in run_analysis script to create tidy data.
+
+1. Create two dataframe by reading training set "X_train.txt"" and test set "X_test.txt"
+
+2. Create acitivity and subject dataframes for train and test set. Following files contains the respective data "y_train.txt", "subject_train.txt", "y_test.txt", "subject_test.txt"
+
+3. Create Activity lables data frame by reading "activity_labels.txt". This dataframe has activity id and activity name mapping
+
+4. Create  feature name data frame by reading "features.txt"
+
+5. Add activity id and Subject id columns in training set as well as test set
+
+6. Create merged dataframe by combining rows from training and testing sets
+
+7. Add column names for each variable using feature name dataframe
+
+8. Tidy up the merged dataset by adding activity labels. Add activity labels to each row by merging the combined data set with activity label dataframe
+
+9. Extract mean and standard deviation columns from the merged data from previous step to create "mean_std_data" dataframe
+
+10. Create a tidy data with average of each variable for each subject for each activity by preforming the following
+ - group "mean_std_data" dataframe by subject and activity name
+ - summarise each variable in group by mean using summarise_all function 
+
+11. write the data frame form previous step to tidy_dataset.txt file
--- run_analysis.R
+++ run_analysis.R
@@ -20,28 +20,31 @@ train_data <- cbind(subjectId=train_subjects$V1,activityId=train_activities$V1,t
 #Add activity id, subject id columns to test set.
 test_data <- cbind(subjectId=test_subjects$V1,activityId=test_activities$V1,test_data)
 
-#Merge training and testing sets and add column names for all variables
+###Merge training and testing sets and tidy up the data###
+
+#1. Combine train and test sets
 merged_train_test_data <- union(train_data,test_data)
+
+#2. Add variable names for each feature column
 names(merged_train_test_data)[-c(1:2)] <- features$V2 
 
-#Tidy up the merged dataset by adding activity labels
-#Add activity labels to each row by merging the combined data set with activity label dataset 
+#3. Add activity labels to each row by merging with activity label dataset 
 tidy_merged_data <- merge(activity_labels,merged_train_test_data,by="activityId")
 
 
-###Extract required variables and summarize the extracted data
+###Extract required variables and summarize the extracted data###
 
 #1. Extract mean and standard deviation data for each activity and subject
 mean_std_data <- tidy_merged_data %>%
                  select(subjectId,activityName,contains("mean()")|contains("std()"))
 
-#2. create a tidy data set with average for each mean() and std() variable for each activity and subject
+#2. Create a tidy data set with average for each mean() and std() variable for each activity and subject
 average_mean_std_data <- mean_std_data %>%
                          group_by(subjectId,activityName)%>%
                          summarise_all(mean)
 
-#3.Rename the variable column to indicate the data represents average of mean() or std()
+#3. Rename the variable column to indicate the data represents average of mean() or std()
 names(average_mean_std_data)[-c(1:2)] <- paste0("Average_",names(average_mean_std_data)[-c(1:2)])
 
-###write summarized data to file
+###write summarized data to file###
 write.table(average_mean_std_data,file="./tidy_dataset.txt")
